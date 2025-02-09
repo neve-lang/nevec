b7d9bce25c99ac8ce5cb995d694a0fdd4bebfd9e
ademyrodev <ademerebaixadp@gmail.com>
2025-02-09 13:37:22 +0100

    tiny oversight

--- nevec/check/sem/sem.py
+++ nevec/check/sem/sem.py
@@ -1,5 +1,6 @@
-from nevec.ast.ast import Ast
-from nevec.ast.visit import Visit
+from nevec.ast.ast import *
+from nevec.check.phase import Phase
 
-class SemCheck(Visit[Ast, bool]):
+class SemCheck(Phase):
     def visit_Program(self, program: Program) -> bool:
+        return self.check_for(*program.decls)
