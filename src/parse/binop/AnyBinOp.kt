b84b6ed46d466b8a3ad7706f4ff66dfae755eb23
ademyrodev <ademerebaixadp@gmail.com>
2025-03-21 17:02:10 +0100

    add docstring and remove unused method

--- src/parse/binop/AnyBinOp.kt
+++ src/parse/binop/AnyBinOp.kt
@@ -5,15 +5,11 @@ import ast.hierarchy.binop.Operator
 import ast.hierarchy.expr.Expr
 import file.span.Loc
 
-class AnyBinOp(val loc: Loc, val left: Expr, val operator: Operator, val right: Expr) {
+/**
+ * Intermediate helper class that simplifies the process of building a [BinOp] node.
+ */
+class AnyBinOp(val loc: Loc, private val left: Expr, private val operator: Operator, private val right: Expr) {
     fun bitwise() = BinOp.Bitwise(left, operator, right, loc)
     fun arith() = BinOp.Arith(left, operator, right, loc)
     fun comp() = BinOp.Comp(left, operator, right, loc)
-}
-
-fun BinOp.abstract() = when (this) {
-    is BinOp.Bitwise -> AnyBinOp(loc, left, operator, right)
-    is BinOp.Comp -> AnyBinOp(loc, left, operator, right)
-    is BinOp.Arith -> AnyBinOp(loc, left, operator, right)
-    is BinOp.Concat -> AnyBinOp(loc, left, Operator.PLUS, right)
 }
