10a8e7721ca50bcd587cb3666204119c43fcbf43
ademyrodev <ademerebaixadp@gmail.com>
2025-04-06 20:15:35 +0200

    tiny chance within MsgBuilder

--- src/evo/Evo.kt
+++ src/evo/Evo.kt
@@ -1,22 +1,21 @@
+/*
 package evo
 
 import check.analys.Analys
-import ast.hierarchy.expr.Expr
-import type.chance.Chance
-import util.extension.zip
+import ast.typeful.TypefulExpr
+import type.chance.ChanceWrapper
 
 sealed class Evo {
-    data class Simple(val transform: Expr) : Evo()
+    data object Id : Evo()
+    data class Transform(val by: TypefulExpr) : Evo()
     data class Combined(val first: Evo, val second: Evo) : Evo()
 
-    fun applied(to: Chance): Chance = when (this) {
-        is Simple -> Analys.visit(transform.zip(to.wrap()))
+    fun applied(to: ChanceWrapper): ChanceWrapper = when (this) {
+        is Id -> to
+        is Transform -> Analys.visit(Pair(transform, to.wrap()))
         is Combined -> first.applied(to).also { second.applied(to) }
     }
 
     operator fun plus(other: Evo) = Combined(this, other)
-
-    companion object {
-        fun id() = Simple(Expr.empty())
-    }
 }
+ */
