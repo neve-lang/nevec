dcf8966f17b3c50cc5662102280d770324b016a5
ademyrodev <ademerebaixadp@gmail.com>
2025-04-08 18:01:03 +0200

    extend StrChances--allow for `Every`

--- src/chance/repr/str/StrChances.kt
+++ src/chance/repr/str/StrChances.kt
@@ -2,16 +2,30 @@ package chance.repr.str
 
 import chance.repr.Chanceful
 
-data class StrChances(val possibilities: HashSet<String> = HashSet()) : Chanceful<String> {
+sealed class StrChances : Chanceful<String> {
+    data class Limited(val possibilities: HashSet<String>) : StrChances()
+    data object Every : StrChances()
+
     companion object {
-        fun from(vararg possibilities: String) = StrChances(possibilities.toHashSet())
+        fun from(vararg possibilities: String) = Limited(possibilities.toHashSet())
     }
 
-    override fun includes(some: String) = possibilities.contains(some)
+    override fun includes(some: String) = when (this) {
+        is Limited -> possibilities.contains(some)
+        is Every -> true
+    }
 
-    override fun map(to: (String) -> String) = StrChances(possibilities.map(to).toHashSet())
+    override fun map(to: (String) -> String): StrChances = when (this) {
+        is Limited -> Limited(possibilities.map(to).toHashSet())
+        is Every -> this
+    }
 
-    override fun equals(other: Any?) = other is StrChances && possibilities == other.possibilities
+    override fun equals(other: Any?) = when {
+        this is Every && other is Every -> true
+        this is Every || other is Every -> false
+        this is Limited && other is Limited -> possibilities == other.possibilities
+        else -> false
+    }
 
-    override fun hashCode() = possibilities.hashCode()
+    override fun hashCode() = javaClass.hashCode()
 }
