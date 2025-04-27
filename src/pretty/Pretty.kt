d31f5088ec42218bfa37ef6d3f8a752c1253f15d
ademyrodev <ademerebaixadp@gmail.com>
2025-04-27 12:32:59 +0200

    (temporarily) remove Pretty support for Access and AccessConst nodes

--- src/pretty/Pretty.kt
+++ src/pretty/Pretty.kt
@@ -34,8 +34,8 @@ object Pretty : Visit<Program, String> {
         is Expr.OfLit -> visitLit(expr.lit)
         is Expr.OfInterpol -> visitInterpol(expr.interpol)
         is Expr.Parens -> visitParens(expr)
-        is Expr.Access -> visitAccess(expr)
-        is Expr.AccessConst -> visitAccessConst(expr)
+        // is Expr.Access -> visitAccess(expr)
+        // is Expr.AccessConst -> visitAccessConst(expr)
         is Expr.Empty -> visitEmpty()
         is Expr.Show -> visitShow(expr)
     }
@@ -102,6 +102,7 @@ object Pretty : Visit<Program, String> {
         return visitExpr(parens.expr).parenthesized()
     }
 
+    /*
     private fun visitAccess(access: Expr.Access): String {
         return access.name
     }
@@ -109,6 +110,7 @@ object Pretty : Visit<Program, String> {
     private fun visitAccessConst(access: Expr.AccessConst): String {
         return access.name
     }
+     */
 
     private fun visitEmpty(): String {
         return "(empty)"
