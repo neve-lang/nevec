ab314c9f14ee1b5da79f1789a6d1cfeeed80e83c
ademyrodev <ademerebaixadp@gmail.com>
2025-04-30 16:23:50 +0200

    add a Loc to Hinted

--- src/type/hinted/Hinted.kt
+++ src/type/hinted/Hinted.kt
@@ -1,9 +1,11 @@
 package type.hinted
 
+import file.span.Loc
 import type.NamedType
 import type.Type
 import type.Wrappable
 import type.kind.TypeKind
+import util.extension.suffixWith
 
 /**
  * Represents a type that was **explicitly hinted by the user**.
@@ -11,11 +13,14 @@ import type.kind.TypeKind
  * This distinction useful for the semantic resolver and type checker, which allows
  * the compiler to provide more accurate error messages.
  *
+ * In the [pretty printer][pretty.PrettyType], these types are represented with a `..?` suffixed to them.
+ *
  * When an [TypeKind.OfHinted] is confirmed to be valid, the type is extracted using [accept].
  *
  * @property type The actual type that was hinted by the user.
+ * @property loc The [Loc] where the type hint was given.
  */
-data class Hinted(val type: Type) : Wrappable, NamedType {
+data class Hinted(val type: Type, val loc: Loc) : Wrappable, NamedType {
     /**
      * Accepts the hinted type as valid.
      *
@@ -25,11 +30,15 @@ data class Hinted(val type: Type) : Wrappable, NamedType {
         return type
     }
 
+    /**
+     * @return whether the type being stored by the [Hinted]
+     */
+
     override fun wrap(): TypeKind {
         return TypeKind.OfHinted(this)
     }
 
     override fun named(): String {
-        return type.named()
+        return type.named().suffixWith("..?")
     }
 }
