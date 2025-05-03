bafddf59a4ab6a1a7df09d242ca21da1c8ebb63c
ademyrodev <ademerebaixadp@gmail.com>
2025-05-03 13:10:16 +0200

    implement GenOrder

---
+++ src/infer/gen/GenOrder.kt
@@ -0,0 +1,22 @@
+package infer.gen
+
+import type.gen.Free
+
+/**
+ * Silly class that takes care of supplying generic types, such as [Quant] types.
+ *
+ * @property n The number of items to “order.”
+ * @property state The previous [GenTracker] state.
+ */
+data class GenOrder(val n: Int, val state: GenTracker) {
+    /**
+     * Provides [n] fresh new [Free] types.
+     *
+     * @return a list of [n] fresh [Free] types.
+     */
+    fun frees(): List<Free> {
+        return (0..n).map {
+            state.newFree()
+        }
+    }
+}
