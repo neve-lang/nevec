765a65b301c70ee9745b1bc7727543c559b4043e
ademyrodev <ademerebaixadp@gmail.com>
2025-05-03 14:42:59 +0200

    implement Target

---
+++ src/meta/target/Target.kt
@@ -0,0 +1,17 @@
+package meta.target
+
+/**
+ * Represents a target attached to some **meta component**, i.e. a [meta assertion][meta.asserts.MetaAssert] or
+ * a meta annotation.
+ *
+ * A target usually maps to a type of node in the AST or parse rule—for example, the [PRIMARY] target only
+ * applies to primary expressions.
+ */
+enum class Target {
+    /**
+     * Denotes a meta component that only applied to **primary expressions**.
+     *
+     * This includes parenthesized expressions.
+     */
+    PRIMARY;
+}
