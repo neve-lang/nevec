ec6c78ae5e66f359d7d523ba44b12ca22c723399
ademyrodev <ademerebaixadp@gmail.com>
2025-05-04 12:40:59 +0200

    update Meta to use mutable lists and implement add

--- src/meta/Meta.kt
+++ src/meta/Meta.kt
@@ -5,13 +5,22 @@ import meta.asserts.MetaAssert
 /**
  * Encapsulates both [meta assertions][MetaAssert] and `meta annotations` into a same data class.
  */
-data class Meta(val asserts: List<MetaAssert<*>>) {
+data class Meta(val asserts: MutableList<MetaAssert<*>>) {
     companion object {
         /**
          * @return A [Meta] data class with empty lists.
          */
         fun empty(): Meta {
-            return Meta(emptyList())
+            return Meta(mutableListOf())
         }
+    }
+
+    /**
+     * Mutates the current [Meta] object and adds [assert] to its list of [asserts].
+     *
+     * @param assert The assert in question.
+     */
+    fun add(assert: MetaAssert<*>) {
+        asserts.add(assert)
     }
 }
