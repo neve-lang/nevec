fbc2448e8ffa06fdff141e87e47face66c789806
ademyrodev <ademerebaixadp@gmail.com>
2025-05-04 12:36:59 +0200

    implement Meta.empty

--- src/meta/Meta.kt
+++ src/meta/Meta.kt
@@ -5,4 +5,13 @@ import meta.asserts.MetaAssert
 /**
  * Encapsulates both [meta assertions][MetaAssert] and `meta annotations` into a same data class.
  */
-data class Meta(val asserts: List<MetaAssert<*>>)
+data class Meta(val asserts: List<MetaAssert<*>>) {
+    companion object {
+        /**
+         * @return A [Meta] data class with empty lists.
+         */
+        fun empty(): Meta {
+            return Meta(emptyList())
+        }
+    }
+}
