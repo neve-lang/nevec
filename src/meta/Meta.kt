22c753e166efe58b08b379113758c71a07cee2ad
ademyrodev <ademerebaixadp@gmail.com>
2025-05-07 16:49:12 +0200

    refactor Meta to use MetaComp

--- src/meta/Meta.kt
+++ src/meta/Meta.kt
@@ -1,34 +1,45 @@
 package meta
 
-import meta.asserts.MetaAssert
+import ast.info.impl.Infoful
+import meta.comp.MetaComp
+import meta.fail.MetaFail
+import meta.result.MetaResult
 import meta.target.Target
 
 /**
- * Encapsulates both [meta assertions][MetaAssert] and `meta annotations` into a same data class.
+ * Encapsulates both [meta assertions][meta.comp.asserts.MetaAssert] and `meta annotations` into a same data class,
+ * using [MetaComp].
+ *
+ * @see MetaComp
  */
-data class Meta(val asserts: MutableList<MetaAssert>) {
+data class Meta(val comps: List<MetaComp>) {
     companion object {
         /**
-         * @return A [Meta] data class with empty lists.
+         * @return A [Meta] data class with an empty list of meta components.
          */
         fun empty(): Meta {
-            return Meta(mutableListOf())
+            return Meta(listOf())
         }
     }
 
     /**
-     * Mutates the current [Meta] object and adds [assert] to its list of [asserts].
+     * Tries to add a [meta component][MetaComp] to the current [Meta], producing a [MetaResult].
      *
-     * @param assert The assert in question.
-     * @param to The node target the [MetaAssert] should be applied to.
+     * @param comp The assert in question.
+     * @param to The node target the [MetaComp] should be applied to.
+     * @param of The node itself.
      *
-     * @throws IllegalArgumentException if [assert]’s target doesn’t match [to].
+     * @return a [MetaResult] with a [MetaFail] if [to] can’t be applied to [comp], a [MetaResult] containing
+     * the new [Meta] otherwise.
      */
-    fun add(assert: MetaAssert, to: Target) {
-        require(assert.appliesTo(to)) {
-            "The given assert does not apply to the given target."
-        }
-
-        asserts.add(assert)
+    fun add(comp: MetaComp, to: Target, of: Infoful) : MetaResult {
+        return if (!comp.appliesTo(to))
+            MetaResult.Fail(
+                MetaFail.Target(comp, of)
+            )
+        else
+            MetaResult.Success(
+                Meta(comps + listOf(comp))
+            )
     }
 }
