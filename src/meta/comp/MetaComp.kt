57894ca37fb050439c37a4ec385e16f66267975d
ademyrodev <ademerebaixadp@gmail.com>
2025-05-07 17:51:40 +0200

    make MetaComp an interface

--- src/meta/comp/MetaComp.kt
+++ src/meta/comp/MetaComp.kt
@@ -2,31 +2,10 @@ package meta.comp
 
 import meta.comp.asserts.MetaAssert
 import meta.target.AppliesTo
-import meta.target.Target
 
 /**
- * Union-like sealed class that can either be a [meta assertion][MetaAssert] or a [meta annotation][MetaAnnot].
+ * Semi-marker interface that includes [AppliesTo] for **meta components**.
+ *
+ * @see MetaAssert
  */
-sealed class MetaComp : AppliesTo {
-    companion object {
-        /**
-         * Wraps a [MetaAssert] into a [MetaComp].
-         */
-        fun from(assert: MetaAssert): MetaComp {
-            return OfAssert(assert)
-        }
-    }
-
-    /**
-     * Wrapper around a [MetaAssert].
-     *
-     * @property assert The meta assertion itself.
-     *
-     * @see MetaAssert
-     */
-    data class OfAssert(val assert: MetaAssert) : MetaComp()
-
-    override fun appliesTo(target: Target) = when (this) {
-        is OfAssert -> assert.appliesTo(target)
-    }
-}
+interface MetaComp : AppliesTo
