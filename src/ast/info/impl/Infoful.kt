e3072444350fcfa5e639b82b281f1103dfec21ba
ademyrodev <ademerebaixadp@gmail.com>
2025-05-07 18:01:55 +0200

    define `info` in Infoful

--- src/ast/info/impl/Infoful.kt
+++ src/ast/info/impl/Infoful.kt
@@ -5,8 +5,12 @@ import ast.info.Info
 /**
  * Interface that includes all [Info]-related interfaces into one.
  *
- * It also provides an [update] method, which produces an identical node to that of the implementor node, except
+ * It also provides a couple new methods:
+ *
+ * - An [update] method, which produces an identical node to that of the implementor node, except
  * with a new [Info] data class.
+ *
+ * - An [info] method, which returns the implementor’s [Info] data class attached to it.
  *
  * @see Info
  */
@@ -15,4 +19,9 @@ interface Infoful : Spanned, Typed, WithMeta {
      * Produces an identical node to that of the implementor node, but with a new [Info][ast.info.Info] data class.
      */
     fun update(new: Info): Infoful
+
+    /**
+     * @return The implementor node’s [Info].
+     */
+    fun info(): Info
 }
