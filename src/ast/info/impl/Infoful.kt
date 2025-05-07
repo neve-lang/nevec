ca060a1df9277b42c6be9d163773c56b5dd16eb8
ademyrodev <ademerebaixadp@gmail.com>
2025-05-07 17:01:51 +0200

    define update in Infoful

--- src/ast/info/impl/Infoful.kt
+++ src/ast/info/impl/Infoful.kt
@@ -1,8 +1,18 @@
 package ast.info.impl
 
+import java.lang.ProcessHandle.Info
+
 /**
  * Interface that includes all [ast.info.Info]-related interfaces into one.
  *
+ * It also provides an [update] method, which produces an identical node to that of the implementor node, except
+ * with a new [Info][ast.info.Info] data class.
+ *
  * @see ast.info.Info
  */
-interface Infoful : Spanned, Typed, WithMeta
+interface Infoful : Spanned, Typed, WithMeta {
+    /**
+     * Produces an identical node to that of the implementor node, but with a new [Info][ast.info.Info] data class.
+     */
+    fun update(new: Info): Infoful
+}
