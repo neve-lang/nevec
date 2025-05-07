a5c57a0e8d5c97ae7943a3d65abaf169a225d3f5
ademyrodev <ademerebaixadp@gmail.com>
2025-05-07 17:03:06 +0200

    fix wrong import

--- src/ast/info/impl/Infoful.kt
+++ src/ast/info/impl/Infoful.kt
@@ -1,14 +1,14 @@
 package ast.info.impl
 
-import java.lang.ProcessHandle.Info
+import ast.info.Info
 
 /**
- * Interface that includes all [ast.info.Info]-related interfaces into one.
+ * Interface that includes all [Info]-related interfaces into one.
  *
  * It also provides an [update] method, which produces an identical node to that of the implementor node, except
- * with a new [Info][ast.info.Info] data class.
+ * with a new [Info] data class.
  *
- * @see ast.info.Info
+ * @see Info
  */
 interface Infoful : Spanned, Typed, WithMeta {
     /**
