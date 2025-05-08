446f041d8b1b7b7ae5345217fd1618e5d2578f67
ademyrodev <ademerebaixadp@gmail.com>
2025-05-08 17:17:15 +0200

    implement Ctx.test

--- src/ctx/Ctx.kt
+++ src/ctx/Ctx.kt
@@ -9,4 +9,16 @@ import cli.CliOptions
  *
  * @property options The CLI options the program was launched with.
  */
-data class Ctx(val options: CliOptions)
+data class Ctx(val options: CliOptions) {
+    companion object {
+        /**
+         * @return A [Ctx] context data class with [options][CliOptions] corresponding to a compiler test.
+         *
+         * @see CliOptions
+         * @see CliOptions.test
+         */
+        fun test(): Ctx {
+            return Ctx(CliOptions.test())
+        }
+    }
+}
