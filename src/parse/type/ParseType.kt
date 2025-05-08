ff084c01fe16655b32771d4bc4e8b6f2b89e9230
ademyrodev <ademerebaixadp@gmail.com>
2025-05-08 19:54:21 +0200

    implement ParseType

---
+++ src/parse/type/ParseType.kt
@@ -0,0 +1,76 @@
+package parse.type
+
+import parse.ctx.ParseCtx
+import tok.TokKind
+import type.Type
+import type.gen.Applied
+import type.gen.Free
+import type.gen.arg.TypeArgs
+
+/**
+ * Helper class that takes care of parsing types.
+ */
+class ParseType(ctx: ParseCtx) {
+    private val window = ctx.window
+    private val typeTable = ctx.typeTable
+
+    fun parse(): Type? {
+        return parseType()
+    }
+
+    private fun parseType() = when (window.kind()) {
+        // TODO: add support for alternative table syntax: [K: V]
+        TokKind.APOSTROPHE -> parseFree()
+        else -> parseNamed()
+    }
+
+    private fun parseFree(): Type? {
+        window.advance()
+        val id = window.take(TokKind.INT) ?: return null
+
+        return Free(
+            id.lexeme.toInt(),
+            0
+        ).covered()
+    }
+
+    private fun parseNamed(): Type? {
+        val name = window.take(TokKind.ID) ?: return null
+        val type = typeTable.find(name.lexeme) ?: return Type.undefined(name.lexeme)
+
+        return parseArgs(after = type)
+    }
+
+    private fun parseArgs(after: Type): Type? {
+        return if (window.match(TokKind.LPAREN)) {
+            parseEachArg(after).let {
+                if (window.match(TokKind.RPAREN))
+                    it
+                else
+                    null
+            }
+        } else {
+            parseEachArg(after)
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun parseEachArg(after: Type): Type? {
+        if (!window.kind().isTypeStarter()) {
+            return after
+        }
+
+        val types = mutableListOf(parseType())
+        while (window.match(TokKind.COMMA)) {
+            types.add(parseType())
+        }
+
+        return if (types.any { it == null })
+            null
+        else
+            Applied(
+                TypeArgs(types.toList() as List<Type>),
+                after
+            ).covered()
+    }
+}
