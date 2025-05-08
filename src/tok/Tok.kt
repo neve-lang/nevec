6c9cdb815b691bcceb54b613f1cb9b60165ea6b8
ademyrodev <ademerebaixadp@gmail.com>
2025-05-08 17:57:59 +0200

    small refactoring over Tok

--- src/tok/Tok.kt
+++ src/tok/Tok.kt
@@ -9,18 +9,50 @@ import file.span.Loc
  */
 class Tok(val kind: TokKind, val lexeme: String, val loc: Loc) {
     companion object {
-        fun eof() = Tok(TokKind.EOF, "", Loc.new())
+        /**
+         * @return A [Tok] with `TokKind.EOF`, an empty [lexeme] and whose [Loc] is [Loc.new].
+         */
+        fun eof(): Tok {
+            return Tok(TokKind.EOF, "", Loc.new())
+        }
     }
 
-    fun isEof() = kind == TokKind.EOF
+    /**
+     * @return whether `this` [Tok]’s [TokKind] is `TokKind.EOF`.
+     */
+    fun isEof(): Boolean {
+        return kind == TokKind.EOF
+    }
 
-    fun isNewline() = kind == TokKind.NEWLINE
+    /**
+     * @return whether `this` [Tok]’s [TokKind] is `TokKind.NEWLINE`.
+     */
+    fun isNewline(): Boolean {
+        return kind == TokKind.NEWLINE
+    }
 
-    fun isErr() = kind == TokKind.ERR
+    /**
+     * @return whether `this` [Tok]’s [TokKind] is `TokKind.ERR`.
+     */
+    fun isErr(): Boolean {
+        return kind == TokKind.ERR
+    }
 
-    fun isOf(other: TokKind) = kind == other
+    /**
+     * @return whether `this` [Tok]’s [TokKind] matches [other].
+     */
+    fun isOf(other: TokKind): Boolean {
+        return kind == other
+    }
 
-    operator fun plus(other: Tok) = (loc + other.loc).build()
+    /**
+     * @return The sum of the **[Locs][Loc]** of both operands, as a ([built][file.span.LocBuilder]) [Loc].
+      */
+    operator fun plus(other: Tok): Loc {
+        return (loc + other.loc).build()
+    }
 
-    override fun toString() = "\"$lexeme\" ($kind) $loc"
+    override fun toString(): String {
+        return "\"$lexeme\" ($kind) $loc"
+    }
 }
