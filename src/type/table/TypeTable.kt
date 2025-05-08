93819d6f0e735e4c5fa4194cbbcee995674d4db1
ademyrodev <ademerebaixadp@gmail.com>
2025-05-08 17:12:44 +0200

    implement TypeTable initialization

--- src/type/table/TypeTable.kt
+++ src/type/table/TypeTable.kt
@@ -1,6 +1,7 @@
 package type.table
 
 import type.Type
+import type.prelude.PreludeTypes
 
 /**
  * Registers named types and allows lookup.
@@ -11,10 +12,25 @@ import type.Type
  * @property table The table storing the types mapped by their full-qualified names.
  */
 data class TypeTable(val table: HashMap<String, Type> = hashMapOf()) {
+    init {
+        registerPrelude()
+    }
+
     /**
      * Registers [type] into the type table.
      */
     fun register(type: Type) {
         table[type.named()] = type
+    }
+
+    /**
+     * @return the [Type] named [name] if such a type exists in the type table, `null` otherwise.
+     */
+    fun find(name: String): Type? {
+        return table[name]
+    }
+
+    private fun registerPrelude() {
+        PreludeTypes.ALL.forEach(::register)
     }
 }
