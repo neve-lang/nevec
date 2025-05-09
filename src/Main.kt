3366c19dedb5107a88a90ee00bd6913065c62e67
ademyrodev <ademerebaixadp@gmail.com>
2025-05-09 21:55:56 +0200

    update Main

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,5 @@
-import cli.CliArgs
-import err.report.Report
-import file.contents.Src
-import java.io.IOException
+import nevec.Nevec
 
 fun main(args: Array<String>) {
-    val (file, cliOptions) = try {
-        CliArgs.parse(args)
-    } catch (e: IllegalArgumentException) {
-        println(e.message)
-        return
-    }
-
-    val (src, lines) = try {
-        Src.read(file)
-    } catch (e: IOException) {
-        Report.fileErr(file)
-        return
-    }
+    Nevec.run(args)
 }
