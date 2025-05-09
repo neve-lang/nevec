1fbad9b479a811266e0e0f6c164260562538627a
ademyrodev <ademerebaixadp@gmail.com>
2025-05-09 20:12:52 +0200

    rewrite Lines.of

--- src/err/help/Lines.kt
+++ src/err/help/Lines.kt
@@ -2,13 +2,49 @@ package err.help
 
 import err.line.Line
 import err.note.Note
+import err.write.Out
 
 /**
- * Helper object: makes the process of building [Line]s from multiple [Note]s easier.
+ * Helper object that makes the process of building [Line]s from multiple [Note]s easier.
  */
 object Lines {
-    fun of(vararg notes: Note) = notes.map {
-        val loc = it.loc
-        Line.builder(loc).add(listOf(it)).withLine(loc.line()).build()
+    /**
+     * @return A list of [Line] from [notes].
+     *
+     * If two [Notes][Note] are on the same line, a unique line with both notes is produced.
+     */
+    fun of(vararg notes: Note): List<Line> {
+        return makeLines(notes.toList())
+    }
+
+    private fun makeLines(notes: List<Note>, produced: List<Line> = emptyList()): List<Line> {
+        if (notes.isEmpty()) {
+            return produced
+        }
+
+        val note = notes.first()
+
+        if (produced.isEmpty()) {
+            val first = listOf(oneLine(note))
+            return makeLines(notes.drop(1), produced = first)
+        }
+
+        val last = produced.last()
+
+        return if (note.line() == last.number) {
+            val new = last.asBuilder().add(listOf(note)).build()
+
+            makeLines(notes.drop(1), produced.dropLast(1) + listOf(new))
+        } else {
+            val new = oneLine(note)
+
+            makeLines(notes.drop(1), produced + listOf(new))
+        }
+    }
+
+    private fun oneLine(from: Note): Line {
+        return from.loc.let {
+            Line.builder(it).add(listOf(from)).withLine(it.line()).build()
+        }
     }
 }
