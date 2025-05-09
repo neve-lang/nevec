0cd4f46b13e68c5caa77a83490cf4a19216f6917
ademyrodev <ademerebaixadp@gmail.com>
2025-05-09 20:10:56 +0200

    add failMsg to CheckAssert

--- src/meta/comp/asserts/CheckAssert.kt
+++ src/meta/comp/asserts/CheckAssert.kt
@@ -1,10 +1,16 @@
 package meta.comp.asserts
 
 import ast.info.Info
+import err.msg.Msg
 
 /**
- * Provides a [checkFor] method that is [meta assertion][MetaAssert]-specific.
+ * Provides two methods:
+ *
+ * - A [checkFor] method that is [meta assertion][MetaAssert]-specific.
+ * - A [failMsg] method when the assertion fails.
  */
 interface CheckAssert {
     fun checkFor(info: Info): Boolean
+
+    fun failMsg(info: Info): Msg
 }
