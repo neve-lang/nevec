67ad4cb5a5367b43c35ac56da32fd5041e8bd026
ademyrodev <ademerebaixadp@gmail.com>
2025-05-14 17:22:06 +0200

    update and rewrite ParseMeta

--- src/parse/meta/ParseMeta.kt
+++ src/parse/meta/ParseMeta.kt
@@ -1,5 +1,6 @@
 package parse.meta
 
+import ast.info.impl.Infoful
 import cli.Options
 import file.span.Loc
 import meta.Meta
@@ -9,6 +10,9 @@ import meta.fail.MetaFail
 import meta.result.MetaResult
 import meta.target.Target
 import parse.ctx.ParseCtx
+import parse.err.ParseErr
+import parse.err.ParseResult
+import parse.help.TinyParse
 import parse.type.ParseType
 import tok.Tok
 import tok.TokKind
@@ -21,29 +25,46 @@ import tok.TokKind
  * @see MetaComp
  * @see MetaResult
  */
-class ParseMeta(private val ctx: ParseCtx) {
-    private val window = ctx.window
-    private val options = ctx.cliCtx.options
-
-    /**
-     * Tries to parse a [meta component][MetaComp].
-     *
-     * @return A [MetaResult.Success] if successful, a [MetaResult.Fail] otherwise.
-     */
-    fun parse(to: Target): MetaResult {
-        if (!options.isEnabled(Options.META_ASSERTS)) {
-            return MetaFail.NotEnabled(window.here()).wrap()
+object ParseMeta : TinyParse<Pair<Infoful, Target>, Meta> {
+    override fun parse(ctx: ParseCtx, data: Pair<Infoful, Target>): ParseResult<Meta> {
+        val allMeta = parseAllMeta(ctx, data)
+        if (allMeta.size == 1) {
+            return ParseResult.Success(allMeta.first().or(Meta.empty()), ctx)
         }
 
-        return if (!window.check(TokKind.META_ASSERT))
-            Meta.empty().wrap()
-        else
-            parseMeta(to)
+        val reduced = allMeta.reduce { a, b -> a + b }
+
+        return when (reduced) {
+            is MetaResult.Success -> ParseResult.Success(reduced.meta, ctx)
+            is MetaResult.Fail -> ParseResult.SemiFail(Meta.empty(), ctx)
+        }
+    }
+    
+    private fun parseAllMeta(ctx: ParseCtx, data: Pair<Infoful, Target>): List<MetaResult> {
+        val (node, to) = data
+        val parsed = parseMeta(ctx, to)
+
+        if (parsed.isDummy()) {
+            return listOf(parsed)
+        }
+
+        if (parsed is MetaResult.Fail) {
+            ParseErr.metaFail(node.loc(), parsed.reason)?.let(ctx::showMsg)
+        }
+
+        return listOf(parsed) + parseAllMeta(ctx, data)
     }
 
-    private fun parseMeta(to: Target): MetaResult {
-        return when (window.kind()) {
-            TokKind.META_ASSERT -> parseAssert(to)
+    private fun parseMeta(ctx: ParseCtx, to: Target): MetaResult {
+        return if (!ctx.check(TokKind.META_ASSERT))
+            MetaFail.Dummy.wrap()
+        else
+            parseMetaComp(ctx, to)
+    }
+
+    private fun parseMetaComp(ctx: ParseCtx, to: Target): MetaResult {
+        return when (ctx.kind()) {
+            TokKind.META_ASSERT -> parseAssert(ctx, to)
 
             else -> throw IllegalArgumentException(
                 "`parseMeta` may only be called when a `TokKind.META_ASSERT` or `TokKind.META_ANNOT`."
@@ -51,25 +72,33 @@ class ParseMeta(private val ctx: ParseCtx) {
         }
     }
 
-    private fun parseAssert(to: Target): MetaResult {
-        val from = window.here()
-        window.advance()
-        val id = window.take(TokKind.ID) ?: return parseFail()
+    private fun parseAssert(ctx: ParseCtx, to: Target): MetaResult {
+        val options = ctx.cliCtx.options
+        if (!options.isEnabled(Options.META_ASSERTS)) {
+            return MetaFail.NotEnabled(ctx.here()).wrap()
+        }
 
-        window.take(TokKind.EQ) ?: return parseFail()
+        val from = ctx.consumeHere()
+        val id = ctx.consume(TokKind.ID) ?: return parseFail(ctx)
+
+        ctx.consume(TokKind.EQ)
 
         return when (id.lexeme) {
-            "type" -> parseTypeAssert(from, to)
+            "type" -> parseTypeAssert(ctx, from, to)
             else -> unknownAssert(id)
         }
     }
 
-    private fun parseTypeAssert(from: Loc, to: Target): MetaResult {
-        val type = ParseType(ctx).parse() ?: return inputFail()
-        val loc = from.tryMerge(with = window.here())
+    private fun parseTypeAssert(ctx: ParseCtx, from: Loc, to: Target): MetaResult {
+        val parsed = ParseType.parse(ctx.new(), Unit)
+
+        val newCtx = parsed.newCtx()
+        val type = parsed.success() ?: return inputFail(ctx)
+
+        val loc = from.tryMerge(with = newCtx.here())
 
         val assert = MetaAssert.TypeAssert(type, loc)
-        closingBracket() ?: return parseFail()
+        closingBracket(newCtx)
 
         return applyOrFail(assert, to, loc)
     }
@@ -85,15 +114,15 @@ class ParseMeta(private val ctx: ParseCtx) {
             MetaFail.Target(at).wrap()
     }
 
-    private fun closingBracket(): Tok? {
-        return window.take(TokKind.RBRACKET)
+    private fun closingBracket(ctx: ParseCtx): Tok? {
+        return ctx.consume(TokKind.RBRACKET)
     }
 
-    private fun parseFail(): MetaResult {
-        return MetaFail.Parse(window.here()).wrap()
+    private fun parseFail(ctx: ParseCtx): MetaResult {
+        return MetaFail.Parse(ctx.here()).wrap()
     }
 
-    private fun inputFail(): MetaResult {
-        return MetaFail.Input(window.here()).wrap()
+    private fun inputFail(ctx: ParseCtx): MetaResult {
+        return MetaFail.Input(ctx.here()).wrap()
     }
 }
