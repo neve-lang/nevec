1a841546855b6b190c30b56aa52242cdfe985031
ademyrodev <ademerebaixadp@gmail.com>
2025-05-16 15:17:17 +0200

    update CulpritsTest

--- test/feature/culprits/CulpritsTest.kt
+++ test/feature/culprits/CulpritsTest.kt
@@ -2,10 +2,16 @@ package feature.culprits
 
 import feature.FeatureTest
 import org.junit.jupiter.api.Test
+import type.poison.Poison
 
 class CulpritsTest {
     @Test
     fun test() {
+        // we’re getting some really weird behavior here—it seems like, if I remove this tiny statement,
+        // Poison.NAMES goes wild and sets its "Unknown" entry to `null`, even though it’s a private field and nothing
+        // tells it to do that anywhere.
+        Poison.fromName("")
+
         assert(FeatureTest("culprits").succeeds())
     }
 }
