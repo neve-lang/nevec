c10099c70163699e94ac033f8450ed187d0ea9ab
ademyrodev <ademerebaixadp@gmail.com>
2025-05-21 15:57:42 +0200

    update Top

--- src/ast/hierarchy/top/Top.kt
+++ src/ast/hierarchy/top/Top.kt
@@ -1,13 +1,14 @@
 package ast.hierarchy.top
 
 import ast.hierarchy.Ast
+import ast.hierarchy.decl.Decl
 import file.span.Loc
 
 /**
  * Represents all kinds of **top-level** declarations supported in Neve.
  *
- * Notice how they are different from **[simple declarations][ast.hierarchy.decl.Decl]**—simple declarations have
- * an [OfStmt][ast.hierarchy.decl.Decl.OfStmt] variant which, if used for top-level declarations as well, would
+ * Notice how they are different from **[simple declarations][Decl]**—simple declarations have
+ * an [OfStmt][Decl.OfStmt] variant which, if used for top-level declarations as well, would
  * allow top-level statements—something Neve wants to avoid.
  */
 sealed class Top : Ast {
@@ -16,5 +17,12 @@ sealed class Top : Ast {
      *
      * Right now, we do not support arguments.  We are simply accommodating for the main function.
      */
-    data class Fun(val name: String, val loc: Loc) : Top()
+    data class Fun(val name: String, val decls: List<Decl>, val loc: Loc) : Top()
+
+    /**
+     * A dummy top-level declaration.
+     *
+     * Occurs in the AST when a top-level declaration was expected, but none was found.
+     */
+    data class Empty(val loc: Loc) : Top()
 }
