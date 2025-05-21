2542fa1057ccd79530c5f335e9bbe293585724f1
ademyrodev <ademerebaixadp@gmail.com>
2025-05-21 15:57:47 +0200

    update Program

--- src/ast/hierarchy/program/Program.kt
+++ src/ast/hierarchy/program/Program.kt
@@ -2,6 +2,7 @@ package ast.hierarchy.program
 
 import ast.hierarchy.Ast
 import ast.hierarchy.decl.Decl
+import ast.hierarchy.top.Top
 
 /**
  * Denotes a Neve program file.
@@ -14,7 +15,7 @@ import ast.hierarchy.decl.Decl
  *
  * @see Decl
  */
-data class Program(val decls: List<Decl>) : Ast {
+data class Program(val decls: List<Top>) : Ast {
     companion object {
         /**
          * @return A [Program] with an [emptyList] of [decls].
