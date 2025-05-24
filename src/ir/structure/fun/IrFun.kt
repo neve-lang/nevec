38f50124050453297f42a1a3a4acf0b42d775eb2
ademyrodev <ademerebaixadp@gmail.com>
2025-05-24 18:19:25 +0200

    implement IrFun

---
+++ src/ir/structure/fun/IrFun.kt
@@ -0,0 +1,34 @@
+package ir.structure.`fun`
+
+import ir.structure.block.Block
+import ir.data.`fun`.FunData
+import ir.data.term.TermData
+import ir.term.TermLike
+
+/**
+ * Represents an IR function.
+ *
+ * An IR function is made of a list of [basic blocks][Block], linked together in a **control flow graph**.
+ *
+ * At the time of writing, Neve does not support branching—therefore, [blocks] will always be singleton.
+ */
+data class IrFun<T : TermLike>(
+    val mangledName: String,
+    val blocks: List<Block<T>>,
+    val termData: TermData<T>,
+    val funData: FunData<T>
+) {
+    companion object {
+        /**
+         * @return A new [IrFun] from a list of basic blocks, with a [name] provided.
+         */
+        fun <T : TermLike> from(blocks: List<Block<T>>, name: String): IrFun<T> {
+            return IrFun(
+                name,
+                blocks,
+                TermData.from(blocks),
+                FunData.from(blocks)
+            )
+        }
+    }
+}
