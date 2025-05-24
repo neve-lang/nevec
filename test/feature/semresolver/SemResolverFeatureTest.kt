9348e9969f65f662a4ff8c220397f71d3b0ca1d1
ademyrodev <ademerebaixadp@gmail.com>
2025-05-24 18:08:31 +0200

    change to assertFalse in SemResolverFeatureTest

--- test/feature/semresolver/SemResolverFeatureTest.kt
+++ test/feature/semresolver/SemResolverFeatureTest.kt
@@ -1,11 +1,12 @@
 package feature.semresolver
 
 import feature.FeatureTest
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 
 class SemResolverFeatureTest {
     @Test
     fun test() {
-        assert(FeatureTest("semresolver").succeeds())
+        assertFalse(FeatureTest("semresolver").succeeds())
     }
 }
