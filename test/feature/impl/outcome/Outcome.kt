a04fa6566403cfcdf3cefb6014701c078e0bd986
ademyrodev <ademerebaixadp@gmail.com>
2025-05-25 14:07:07 +0200

    implement Outcome

---
+++ test/feature/impl/outcome/Outcome.kt
@@ -0,0 +1,24 @@
+package feature.impl.outcome
+
+/**
+ * Represents a possible outcome when executing a test.
+ *
+ * It can either be:
+ *
+ * - Successful: [Success]
+ * - Unsuccessful: [Fail]
+ *
+ * Keep in mind that an unsuccessful outcome **does not imply** that the **test itself failed**—it is perfectly common
+ * to expect a test to fail.
+ */
+sealed class Outcome {
+    /**
+     * Represents a successful test execution.
+     */
+    data object Success : Outcome()
+
+    /**
+     * Represents an unsuccessful test execution, containing a [FailStage].
+     */
+    data class Fail(val stage: FailStage) : Outcome()
+}
