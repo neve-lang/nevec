1fdbade957a2f32bedc018bd474eb237cc577a14
ademyrodev <ademerebaixadp@gmail.com>
2025-05-25 14:09:01 +0200

    define FileId

---
+++ test/feature/impl/file/FileId.kt
@@ -0,0 +1,7 @@
+package feature.impl.file
+
+/**
+ * Simple type alias that represents a **file ID**—the number that appears before the `.neve` extension in test files
+ * found in `neve-test/`.
+ */
+typealias FileId = Int
