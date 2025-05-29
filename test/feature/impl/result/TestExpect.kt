e7a098a6a07e7b3549a5c0d2169d698dd76edb00
ademyrodev <ademerebaixadp@gmail.com>
2025-05-29 20:02:55 +0200

    implement TestExpect.from and compare

--- test/feature/impl/result/TestExpect.kt
+++ test/feature/impl/result/TestExpect.kt
@@ -2,6 +2,7 @@ package feature.impl.result
 
 import feature.impl.file.FileId
 import feature.impl.outcome.Outcome
+import feature.impl.outcome.Summary
 
 /**
  * Similarly to [TestResult], it represents an **expected** map of [Outcome] that gets compared to the
@@ -10,4 +11,51 @@ import feature.impl.outcome.Outcome
 data class TestExpect(
     private val expected: Map<FileId, Outcome> = mapOf()
 ) {
+    companion object {
+        /**
+         * @return A new [TestExpect] whose [expected] has the following properties:
+         *
+         * - For the file IDs mentioned, those will be mapped to an [Outcome] of
+         *   [COMPILE][feature.impl.outcome.ExecFail.COMPILE] failure.
+         * - The non-mentioned ones will, up to the given [testCount], be implicitly given an [Outcome] of
+         *   [Success][Outcome.Success].
+         *
+         * @throws IllegalArgumentException if [testCount] is less than `0`.
+         */
+        fun failuresAt(testCount: Int, vararg failures: FileId): TestExpect {
+            require(testCount >= 0) {
+                "`TestExpect.failuresAt` requires a strictly positive `fileCount`."
+            }
+
+            return (0..testCount).associateWith {
+                Outcome.basedOn(whether = it !in failures)
+            }.toMap().let(::TestExpect)
+        }
+    }
+
+    /**
+     * Compares `this` [TestExpect] with a [TestResult].
+     *
+     * @return A [Summary] with the discrepancies stored there.
+     */
+    fun compare(with: TestResult): Summary {
+        return discrepancies(expected, with.outcomes)
+            .groupBy { (_, outcome) -> outcome }
+            .mapValues { (_, grouped) -> grouped.map { it.first } }
+            .let(::Summary)
+    }
+
+    private fun discrepancies(
+        a: Map<FileId, Outcome>,
+        b: Map<FileId, Outcome>
+    ): List<Pair<FileId, Outcome>> {
+        val aAsList = a.toList()
+        val bAsList = b.toList()
+
+        return aAsList.associateBy { (fileId, _) -> fileId }.let { map ->
+            bAsList.filter { (fileId, outcome) ->
+                outcome != map[fileId]?.second
+            }
+        }
+    }
 }
