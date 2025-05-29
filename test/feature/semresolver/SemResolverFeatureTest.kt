b5f12db178b93c56db1223a13d0a70cd45e0a612
ademyrodev <ademerebaixadp@gmail.com>
2025-05-29 20:18:47 +0200

    update SemResolverFeatureTest

--- test/feature/semresolver/SemResolverFeatureTest.kt
+++ test/feature/semresolver/SemResolverFeatureTest.kt
@@ -1,12 +1,22 @@
 package feature.semresolver
 
-import feature.FeatureTest
+import feature.impl.FeatureTest
+import feature.impl.result.ExpectMap
+import feature.impl.result.TestExpect
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 
 class SemResolverFeatureTest {
     @Test
     fun test() {
-        assertFalse(FeatureTest("semresolver").succeeds())
+        val parseExpect = TestExpect.failsAt(
+            testCount = 19,
+            4, 6, 7, 9, 11, 14, 15, 16
+        )
+
+        assertFalse(FeatureTest(
+            "semresolver",
+            ExpectMap.forParseTests(parseExpect)
+        ).check())
     }
 }
