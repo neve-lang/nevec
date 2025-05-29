db251d761fab1eefcf05185233ac9717715016b6
ademyrodev <ademerebaixadp@gmail.com>
2025-05-29 20:01:28 +0200

    implement FileId.from

--- test/feature/impl/file/FileId.kt
+++ test/feature/impl/file/FileId.kt
@@ -1,7 +1,19 @@
 package feature.impl.file
 
+import java.nio.file.Path
+import kotlin.io.path.nameWithoutExtension
+
 /**
  * Simple type alias that represents a **file ID**—the number that appears before the `.neve` extension in test files
  * found in `neve-test/`.
  */
 typealias FileId = Int
+
+/**
+ * @return A new [FileId] from a file’s full [Path].
+ */
+fun FileIdCompanion.from(path: Path): FileId {
+    return path.nameWithoutExtension.toInt()
+}
+
+private typealias FileIdCompanion = Int.Companion
