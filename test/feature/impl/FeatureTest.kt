917bac00bcc8011495ef3245d30f58afaaf7ea79
ademyrodev <ademerebaixadp@gmail.com>
2025-05-29 20:03:49 +0200

    update FeatureTest

--- test/feature/impl/FeatureTest.kt
+++ test/feature/impl/FeatureTest.kt
@@ -1,5 +1,11 @@
 package feature.impl
 
+import feature.impl.file.FileId
+import feature.impl.file.from
+import feature.impl.outcome.Outcome
+import feature.impl.result.ExpectMap
+import feature.impl.result.TestExpect
+import feature.impl.result.TestResult
 import feature.impl.routine.TestRoutine
 import java.io.IOException
 import java.nio.file.Path
@@ -9,8 +15,23 @@ import kotlin.io.path.*
  * Takes care of feature tests by testing the entire test folder.  The folder is located using the name of the feature.
  *
  * @param name The name of the feature to be tested.
+ * @param expects The [TestExpects][TestExpect] given by the user, **mapped by folder name**.
  */
-class FeatureTest(private val name: String) {
+class FeatureTest(
+    private val name: String,
+    private val expects: ExpectMap
+) {
+    init {
+        require(expects.folders().all { isExpectedFolder(it) }) {
+            """
+            The given `ExpectMap` must only contain **expected** folder names.
+            
+            The expected names include:
+            - parse
+            """.trimIndent()
+        }
+    }
+
     companion object {
         /**
          * Maps test folder names to [TestRoutines][TestRoutine].
@@ -29,21 +50,29 @@ class FeatureTest(private val name: String) {
             emptyList()
         }
 
-        return folders.flatMap(::testFolder).all { it }
-    }
-
-    private fun testFolder(folder: Path): List<Boolean> {
-        println(" → Testing ${folder.name} folder:")
-
-        return folder.listDirectoryEntries().sortedBy {
-            it.nameWithoutExtension.toInt()
+        return folders.map {
+            expects[it.name]!! to testFolder(it)
         }.map {
-            test(it)
+            (expected, actual) -> expected.compare(with = actual)
+        }.all {
+            summary -> !summary.hasDiscrepancies().also { println(summary.msg()) }
         }
     }
 
-    private fun test(file: Path): Boolean {
-        return TestHook.okay(argsFor(file))
+    private fun testFolder(folder: Path): TestResult {
+        println(" → Testing `${folder.name}` folder:")
+
+        return folder.listDirectoryEntries().sortedBy {
+            FileId.from(it)
+        }.map {
+            test(it, from = folder)
+        }.let(TestResult::from)
+    }
+
+    private fun test(file: Path, from: Path): Pair<FileId, Outcome> {
+        return TEST_ROUTINES[from.name]!!.let {
+            routine -> FileId.from(file) to routine.run(file)
+        }
     }
 
     private fun open(): List<Path> {
@@ -55,7 +84,7 @@ class FeatureTest(private val name: String) {
 
         return folder
             .listDirectoryEntries()
-            .filter { it.name in TEST_ROUTINES.keys }
+            .filter { isExpectedFolder(it.name) }
             .map(Path::toRealPath)
     }
 
@@ -63,14 +92,8 @@ class FeatureTest(private val name: String) {
         return Path("neve-test/feature/${name}/")
     }
 
-    private fun argsFor(filename: Path): Array<String> {
-        return arrayOf(
-            filename.toString(),
-            "--meta-asserts",
-            "--compiler-types",
-            "--culprits",
-            "--no-opt"
-        )
+    private fun isExpectedFolder(name: String): Boolean {
+        return name in TEST_ROUTINES.keys
     }
 
     private fun fail() {
