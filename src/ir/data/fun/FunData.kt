df12c39562c52b0166f539f28eb40cee1dfb5a32
ademyrodev <ademerebaixadp@gmail.com>
2025-06-03 16:04:01 +0200

    update FunData

--- src/ir/data/fun/FunData.kt
+++ src/ir/data/fun/FunData.kt
@@ -1,6 +1,6 @@
 package ir.data.`fun`
 
-import ir.data.change.FunChange
+import ir.data.change.Change
 import ir.data.term.TermData
 import ir.structure.block.Block
 import ir.structure.consts.IrConst
@@ -13,46 +13,34 @@ import ir.term.TermLike
  * IrConsts of the same value stored.
  */
 data class FunData<T : TermLike>(
-    val constDefMap: Map<IrConst, List<T>>,
-    val termData: TermData<T>
+    val constDefMap: Map<IrConst, List<T>> = emptyMap(),
+    val termData: TermData<T> = TermData.new()
 ) {
     companion object {
-        /**
-         * @return A new [FunData] with the following properties:
-         *
-         * - An empty map for [constDefMap]
-         * - A fresh new [TermData] for [termData]
-         */
-        fun <T : TermLike> new(): FunData<T> {
-            return FunData(
-                constDefMap = emptyMap(),
-                termData = TermData.new()
-            )
-        }
-
         /**
          * @return A new [FunData] whose properties were derived from the list of [Blocks][Block].
          */
         fun <T : TermLike> from(blocks: List<Block<T>>): FunData<T> {
             val ops = blocks.flatMap { it.ops }
-            val changes = ops.flatMap { FunChange.deriveFrom(it) }
+            val changes = ops.flatMap { Change.deriveFrom(it) }
 
             return updateAll(changes)
         }
 
-        private fun <T : TermLike> updateAll(changes: List<FunChange<T>>): FunData<T> {
+        private fun <T : TermLike> updateAll(changes: List<Change<T>>): FunData<T> {
             return if (changes.isEmpty())
-                new()
+                FunData()
             else
                 changes.first().applyTo(updateAll(changes.drop(1)))
         }
     }
 
     /**
-     * @return The [List] of [T] that maps the given [const] in [constDefMap], or `null` if the entry does not exist.
+     * @return The [List] of [T] that maps the given [const] in [constDefMap], or an empty list if the entry does not
+     * exist.
      */
-    fun termsUsing(const: IrConst): List<T>? {
-        return constDefMap[const]
+    fun termsUsing(const: IrConst): List<T> {
+        return constDefMap[const] ?: emptyList()
     }
 
     /**
