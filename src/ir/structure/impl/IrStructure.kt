9261246a71261fdd698a4a8351458c682d9456a1
ademyrodev <ademerebaixadp@gmail.com>
2025-06-07 11:50:09 +0200

    define IrStructure

---
+++ src/ir/structure/impl/IrStructure.kt
@@ -0,0 +1,8 @@
+package ir.structure.impl
+
+/**
+ * Marker interface that represents IR structures, such as IR ops and blocks.
+ *
+ * It is useful, because it allows us to use generic types that only apply to IR structures.
+ */
+interface IrStructure
