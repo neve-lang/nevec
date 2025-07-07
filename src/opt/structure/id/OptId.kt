d10999719c31523d50582e7ead159a5712eb5723
ademyrodev <ademerebaixadp@gmail.com>
2025-07-07 20:41:11 +0200

    define OptId

---
+++ src/opt/structure/id/OptId.kt
@@ -0,0 +1,15 @@
+package opt.structure.id
+
+/**
+ * Represents an optimization pass’s ID.
+ *
+ * For each distinct optimization pass, there exists a distinct [OptId].
+ *
+ * [OptIds][OptId] allow us to disable certain optimization passes.
+ */
+enum class OptId {
+    /**
+     * The constant-folding optimization pass ID.
+     */
+    CONST_FOLD
+}
