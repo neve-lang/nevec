5c1584661079f256e0e10d39e770ec1e2e35e92d
ademyrodev <ademerebaixadp@gmail.com>
2025-07-07 23:46:27 +0200

    implement Twice

---
+++ src/util/twice/Twice.kt
@@ -0,0 +1,20 @@
+package util.twice
+
+/**
+ * Represents a tuple where both elements have the same type.
+ */
+typealias Twice<A> = Pair<A, A>
+
+/**
+ * Coerces a [List] into a [Twice].
+ *
+ * @return A [Twice] with the two first elements of the list of the list’s length is greater or equal to 2.
+ * @throws IllegalArguemntException if the list is smaller than that.
+ */
+fun <T> List<T>.twice(): Twice<T> {
+    if (isEmpty()) {
+        throw IllegalArgumentException("List has too few elements to be turned into a `Twice` (min. 2)")
+    }
+
+    return take(2).let { (a, b) -> Twice(a, b) }
+}
