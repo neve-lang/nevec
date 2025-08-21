c88cf2abaa6ab55090a55fce6c75184863404aa0
ademyrodev <ademerebaixadp@gmail.com>
2025-08-21 06:16:27 +0200

    define MainFun in CSR

--- src/csr/hierarchy/top/CsrTop.kt
+++ src/csr/hierarchy/top/CsrTop.kt
@@ -4,5 +4,6 @@ import csr.hierarchy.expr.CsrExpr
 import csr.type.CsrType
 
 sealed class CsrTop {
-    data class CsrFun(val name: String, val body: CsrExpr, val type: CsrType) : CsrTop()
+    data class MainFun(val body: CsrExpr) : CsrTop()
+    data class Fun(val name: String, val body: CsrExpr, val type: CsrType) : CsrTop()
 }
