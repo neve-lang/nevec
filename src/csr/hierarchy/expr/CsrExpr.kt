a0f663bc1ce3dc5b8f759d87a57598c7ce0d5914
ademyrodev <ademerebaixadp@gmail.com>
2025-08-21 06:27:18 +0200

    add temporary `Print` expression

--- src/csr/hierarchy/expr/CsrExpr.kt
+++ src/csr/hierarchy/expr/CsrExpr.kt
@@ -6,12 +6,15 @@ import csr.hierarchy.unop.CsrUnOp
 import type.Type
 
 sealed class CsrExpr {
+    data class Print(val expr: CsrExpr) : CsrExpr()
+
     data class Parens(val expr: CsrExpr) : CsrExpr()
     data class OfUnOp(val unOp: CsrUnOp, val type: Type) : CsrExpr()
     data class OfBinOp(val binOp: CsrBinOp, val type: Type) : CsrExpr()
     data class OfLit(val lit: CsrLit, val type: Type) : CsrExpr()
 
     fun type(): Type = when (this) {
+        is Print -> expr.type()
         is Parens -> expr.type()
         is OfUnOp -> type
         is OfBinOp -> type
