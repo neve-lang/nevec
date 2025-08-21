7db418dcd067acde5a3545801bb43ceb08ad5cab
ademyrodev <ademerebaixadp@gmail.com>
2025-08-21 06:16:46 +0200

    add type information to CsrBinOp

--- src/csr/hierarchy/binop/CsrBinOp.kt
+++ src/csr/hierarchy/binop/CsrBinOp.kt
@@ -5,10 +5,34 @@ import ast.hierarchy.binop.operator.BitwiseOperator
 import ast.hierarchy.binop.operator.CompOperator
 import ast.hierarchy.binop.operator.ConcatOperator
 import csr.hierarchy.expr.CsrExpr
+import type.Type
 
 sealed class CsrBinOp {
-    data class Bitwise(val left: CsrExpr, val operator: BitwiseOperator, val right: CsrExpr) : CsrBinOp()
-    data class Arith(val left: CsrExpr, val operator: ArithOperator, val right: CsrExpr) : CsrBinOp()
-    data class Comp(val left: CsrExpr, val operator: CompOperator, val right: CsrExpr) : CsrBinOp()
-    data class Concat(val left: CsrExpr, val operator: ConcatOperator, val right: CsrExpr) : CsrBinOp()
+    data class Bitwise(
+        val left: CsrExpr,
+        val operator: BitwiseOperator,
+        val right: CsrExpr,
+        val type: Type
+    ) : CsrBinOp()
+
+    data class Arith(
+        val left: CsrExpr,
+        val operator: ArithOperator,
+        val right: CsrExpr,
+        val type: Type
+    ) : CsrBinOp()
+
+    data class Comp(
+        val left: CsrExpr,
+        val operator: CompOperator,
+        val right: CsrExpr,
+        val type: Type
+    ) : CsrBinOp()
+
+    data class Concat(
+        val left: CsrExpr,
+        val operator: ConcatOperator,
+        val right: CsrExpr,
+        val type: Type
+    ) : CsrBinOp()
 }
