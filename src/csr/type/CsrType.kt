175594d90b8266d4f8e7e16b41a829d0fedbfb55
ademyrodev <ademerebaixadp@gmail.com>
2025-09-02 16:51:53 +0200

    add CsrApplied to CsrType

--- src/csr/type/CsrType.kt
+++ src/csr/type/CsrType.kt
@@ -1,8 +1,10 @@
 package csr.type
 
+import csr.type.applied.CsrApplied
 import csr.type.atom.CsrAtom
 
 sealed class CsrType {
     data class Atom(val atom: CsrAtom) : CsrType()
+    data class Applied(val applied: CsrApplied) : CsrType()
     data object Unit : CsrType()
 }
